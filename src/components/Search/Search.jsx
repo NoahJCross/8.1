eda9de77173bf5e208140c93905fb0b1611db1d7
Noah Cross <s223226235@deakin.edu.au>
2024-09-14 22:01:32 +1000

    Initial Commit

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import "./search.css";
 import { FaSearch } from "react-icons/fa";
 
-const Search = ({ handleSearch, className, ...rest }) => {
+const Search = ({ onSearch }) => {
   const handleChange = (event) => {
     event.preventDefault();
-    handleSearch(event.target.value);
+    onSearch(event.target.value);
   };
 
   return (
-    <form className={`app__search ${className && className}`} {...rest}>
+    <form className={`app__search`}>
       <input type="text" placeholder="Search..." onChange={handleChange} className="search-input" />
       <button type="submit" className="search-button">
         <FaSearch size={20} />
